2bde8f8cb91609446fa855795425e4de4b026b99
TruongThanh <thanhdoken113@gmail.com>
2024-05-29 14:47:25 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componets/home/home.component';
+import { ShopComponent } from './componets/shop/shop.component';
+import { ContactComponent } from './componets/contact/contact.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'shop', component: ShopComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'product-detail', component: ProductDetailComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { HomeComponent } from './componets/home/home.component';
+import { ShopComponent } from './componets/shop/shop.component';
+import { ContactComponent } from './componets/contact/contact.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopComponent,
+    ContactComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
